32556c51d426fa4968e30abe14305a38ef05057b
Conqueror14M <dy6579@srmist.edu.in>
2021-08-13 23:05:56 +0530

    ADD: firebase condig

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhHOLajNWlWFZkqVfqifc9bWl2lsHbZR4",
+    authDomain: "clone-135e2.firebaseapp.com",
+    projectId: "clone-135e2",
+    storageBucket: "clone-135e2.appspot.com",
+    messagingSenderId: "574104138400",
+    appId: "1:574104138400:web:e9d35f93d7c4e1dfca4fc5"
+  };
