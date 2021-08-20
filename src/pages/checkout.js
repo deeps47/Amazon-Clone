dcf24c6e7b088337461b49f4a2f13a521c291226
Conqueror14M <dy6579@srmist.edu.in>
2021-08-20 21:42:24 +0530

    ADD: right side checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,12 +1,16 @@
 import Header from "../components/Header";
 import Image from "next/image"
 import { useSelector } from "react-redux";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "./checkoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/client";
 
 function checkout() {
 
     const items = useSelector(selectItems)
+    const total = useSelector(selectTotal)
+    const [session] = useSession()
 
     return (
         <div className="bg-gray-100">
@@ -33,6 +37,21 @@ function checkout() {
                          hasPrime={item.hasPrime}
                          price={item.price} />))}
                     </div>
+                </div>
+
+                <div className="flex flex-col bg-white p-10 shadow-md">
+                    {items.length > 0 && (
+                        <>
+                            <h2 className="whitespace-nowrap">Subtotal ({items.length} items):{" "}
+                            <span className="font-bold">
+                                <Currency quantity={total} currency = "GBP" />
+                            </span>
+                            </h2>
+                            <button disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+                                {!session ? "Sign in to checkout" : "Proceed"}
+                            </button>
+                        </>
+                    )}
                 </div>
             </main>
         </div>
